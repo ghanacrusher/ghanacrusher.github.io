22d9666ac50032dc915a8ebd4a0f27a708061486
ghanacrusher <zenith@crusherproduct.net>
2017-01-16 17:09:29 +0800

    Update chat.js

--- js/chat.js
+++ js/chat.js
@@ -2,4 +2,4 @@ document.writeln("<div style=\"width:160px;height:245px;float:right; z-index:999
 document.writeln("<div id=\"lpButDivID-1331955599520\"></div>");
 document.writeln("<script language=\"javascript\" src=\"http://pkt.zoosnet.net/JS/LsJS.aspx?siteid=PKT21646611&float=0&lng=en\"></script>");
 document.writeln("<a href=\"javascript:void(0)\" onclick=openZoosUrl(\'chatwin\')  target=\"_self\"><img src=\"images/chat.gif\" border=\"0\"></a>");
-document.writeln("</div>");
+document.writeln("</div>"); 
